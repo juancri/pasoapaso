0ffdf429ee87ed9bbe057d27dfd7f0ccb7a82f01
JC Olivares <juancri@juancri.com>
2021-07-16 13:19:12 -0500

    Adding message about the current phase Adding details about cases

--- src/actions/desplazarse.ts
+++ src/actions/desplazarse.ts
@@ -1,9 +1,17 @@
 
 import { EntityFactory } from "../types";
 
+const PHASE_MESSAGES = new Map<number, string>([
+	[1, "Tu comuna está en cuarentena"],
+	[2, "Tu comuna está en transición"],
+	[3, "Tu comuna está en preparación"],
+	[4, "Tu comuna está en apertura"]
+]);
+
 export default async function* desplazarse(factory: EntityFactory): AsyncIterable<string> {
 
 	const comuna = await factory.requestComuna();
+	yield PHASE_MESSAGES.get(comuna.phase) as string;
 	if (comuna.phase > 2) {
 		// Not in quarantine
 		yield "Libre desplazamiento";
@@ -31,8 +39,8 @@ export default async function* desplazarse(factory: EntityFactory): AsyncIterabl
 	// Vaccinated
 	const hasPass = await factory.requestMobilityPass();
 	if (!hasPass)
-		yield "Obtén tu pase de movilidad";
+		yield "Obtén tu pase de movilidad para tener libre desplazamiento";
 
 	// Has pass
-	yield "Libre desplazamiento";
+	yield "Libre desplazamiento con pase de movilidad";
 }
